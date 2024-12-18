0180b3fb1a44b039e4ba05bb48ae6103a49124ed
rhys706 <rhysmccormack.business@gmail.com>
2024-12-18 04:23:10 -0500

    Resource constraints added

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import pygame
 def main():
     pygame.init()
 
+    timer = pygame.time.Clock()
+    dt = 0
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -17,6 +20,9 @@ def main():
 
         screen.fill((0,0,0))
         pygame.display.flip()
+        
+        raw_dt = timer.tick(60)
+        dt = raw_dt / 1000
 
 if __name__ == "__main__":
     main()  
